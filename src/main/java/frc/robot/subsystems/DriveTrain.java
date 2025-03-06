0db462bf2c9e9c4d2800c6468bac39a9978ea7ed
Carson Angell <carsonjesseangell@gmail.com>
2025-03-06 00:13:21 -0700

    Updated drive train to sparks

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,46 +1,45 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 public class DriveTrain {
     //IDS and control mode for the motor controllers
-    private static final int BACK_LEFT_ID = 0;
+    private static final int BACK_LEFT_ID = 1;
     private static final int FRONT_LEFT_ID = 2;
     private static final int BACK_RIGHT_ID = 3;
     private static final int FRONT_RIGHT_ID = 4;
-    private static final ControlMode MOTOR_CONTROL_MODE = ControlMode.PercentOutput;
 
-    private VictorSP backLeftMotor;
-    private VictorSPX frontLeftMotor;
-    private TalonSRX backRightMotor;
-    private VictorSPX frontRightMotor;
+    private SparkMax backLeftMotor;
+    private SparkMax frontLeftMotor;
+    private SparkMax backRightMotor;
+    private SparkMax frontRightMotor;
+
+    private SparkMaxConfig invertedConfig;
 
     private MecanumDrive drive;
 
     public DriveTrain(){
         //Initializes motors with their respective CAN and PWM ids
-        this.backLeftMotor = new VictorSP(BACK_LEFT_ID);
-        this.frontLeftMotor = new VictorSPX(FRONT_LEFT_ID);
-        this.backRightMotor = new TalonSRX(BACK_RIGHT_ID);
-        this.frontRightMotor = new VictorSPX(FRONT_RIGHT_ID);
+        this.backLeftMotor = new SparkMax(BACK_LEFT_ID, MotorType.kBrushless);
+        this.frontLeftMotor = new SparkMax(FRONT_LEFT_ID, MotorType.kBrushless);
+        this.backRightMotor = new SparkMax(BACK_RIGHT_ID, MotorType.kBrushless);
+        this.frontRightMotor = new SparkMax(FRONT_RIGHT_ID, MotorType.kBrushless);
+
+        invertedConfig = new SparkMaxConfig();
+        invertedConfig.inverted(true);
 
         //Inverts the right side motors
-        frontRightMotor.setInverted(true);
-        backRightMotor.setInverted(true);
+        frontRightMotor.configure(invertedConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+        backRightMotor.configure(invertedConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
 
         //Initializes the mecanum drive with the needed set methods
-        this.drive = new MecanumDrive(
-            (double power) -> frontLeftMotor.set(MOTOR_CONTROL_MODE, power),
-            (double power) -> backLeftMotor.set(power), 
-            (double power) -> frontRightMotor.set(MOTOR_CONTROL_MODE, power), 
-            (double power) -> backRightMotor.set(MOTOR_CONTROL_MODE, power)
-        );
+         this.drive = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor);
     }
 
     /**
@@ -53,7 +52,9 @@ public class DriveTrain {
         drive.driveCartesian(-ySpeed, xSpeed, zRotation);
     }
 
-    // public void drivePolar(double magnitude, Rotation2d angle, double zRotation){
-    //     drive.drivePolar(magnitude, angle, zRotation);
-    // }
+    //Gyroscope needed for polar driving!!
+    //todo - Make this work
+   //  public void drivePolar(double magnitude, Rotation2d angle, double zRotation){
+   //      drive.drivePolar(magnitude, angle, zRotation);
+   //  }
 }
